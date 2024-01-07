5fc422b44d04ad2389486a1f4681afbc0629b0b1
Tianchu Ji <tianchu.ji@stonybrook.edu>
2024-01-07 16:18:12 -0500

    add instruction location into pred, save them in json

--- pred.py
+++ pred.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import numpy as np
 import random
 import argparse
+import shortuuid as su
 from llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
 def parse_args(args=None):
@@ -63,7 +64,7 @@ def get_pred(model, tokenizer, data, max_length, max_gen, prompt_format, dataset
         input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device)
         context_length = input.input_ids.shape[-1]
         print(f"curr_len: {context_length}")
-        if context_length > 6000:
+        if context_length > 5000:
             continue
         if dataset == "samsum": # prevent illegal output on samsum (model endlessly repeat "\nDialogue"), might be a prompting issue
             output = model.generate(
@@ -101,6 +102,89 @@ def get_pred(model, tokenizer, data, max_length, max_gen, prompt_format, dataset
                 num_beams=1,
                 do_sample=False,
                 temperature=1.0,
+            )[0]
+        pred = tokenizer.decode(output[context_length:], skip_special_tokens=True)
+        pred = post_process(pred, model_name)
+        preds.append({"pred": pred, "answers": json_obj["answers"], "all_classes": json_obj["all_classes"], "length": json_obj["length"]})
+        insts_counter += 1
+        if num_insts > 0 and insts_counter == num_insts:
+            break
+
+    return preds
+
+def get_pred_sliced(model, tokenizer, data, max_length, max_gen, prompt_format_struct, prompt_format_compos, dataset, device, model_name, deepspeed=False, num_insts=-1):
+    preds = []
+    insts_counter = 0
+    for json_obj in tqdm(data):
+        data_all_comps = {**json_obj, **prompt_format_compos}
+        # get lengths of the components
+        compos_len = {k: len(tokenizer(data_all_comps[k], truncation=False, return_tensors="pt").input_ids[0]) \
+                      for k in prompt_format_struct["involved_members"]}
+        # compute start and end of components
+        compos_pos, curr_end = {}, -1
+        for k in compos_len.keys():
+            compos_start = curr_end + 1
+            compos_end = compos_start + compos_len[k] - 1
+            compos_pos[k] = (compos_start, compos_end)
+            curr_end = compos_end
+        prompt = prompt_format_struct["struct"].format(**data_all_comps)
+        # truncate to fit max_length (we suggest truncate in the middle, since the left and right side may contain crucial instructions)
+        tokenized_prompt = tokenizer(prompt, truncation=False, return_tensors="pt").input_ids[0]
+        if len(tokenized_prompt) > max_length:
+            half = int(max_length/2)
+            prompt = tokenizer.decode(tokenized_prompt[:half], skip_special_tokens=True)+tokenizer.decode(tokenized_prompt[-half:], skip_special_tokens=True)
+        if dataset not in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]: # chat models are better off without build prompts on these tasks
+            prompt = build_chat(tokenizer, prompt, model_name)
+        input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device)
+        context_length = input.input_ids.shape[-1]
+        print(f"curr_len: {context_length}")
+        if context_length > 5000:
+            continue
+        if dataset == "samsum": # prevent illegal output on samsum (model endlessly repeat "\nDialogue"), might be a prompting issue
+            output = model.generate(
+                **input,
+                max_new_tokens=max_gen,
+                num_beams=1,
+                do_sample=False,
+                temperature=1.0,
+                min_length=context_length+1,
+                eos_token_id=[tokenizer.eos_token_id, tokenizer.encode("\n", add_special_tokens=False)[-1]],
+            )[0]
+        else:
+            if deepspeed:
+                import deepspeed
+                ds_engine = deepspeed.init_inference(
+                    model,
+                    mp_size=1,
+                    dtype=torch.half,
+                    checkpoint=None,
+                    replace_with_kernel_inject=True,
+                    zero = {
+                        "stage": 3,
+                        "offload_param": {
+                            "device": "cpu",
+                            "pin_memory": True
+                        },
+                        "overlap_comm": False
+                    }
+                )
+                model = ds_engine.module
+
+            # create a random inst id and save slice info
+            attn_path = "/chronos_data/tji/.huggingface_cache/transformers/chatglm2-6b-32k-attn-bfp20-1e-3-sta/"
+            inst_id = su.random(length=8)
+            with open(attn_path + f"i{inst_id}.json", "w") as f:
+                json.dump(compos_pos, f)
+                
+
+            output = model.generate(
+                **input,
+                max_new_tokens=max_gen,
+                num_beams=1,
+                do_sample=False,
+                temperature=1.0,
+                attn_dump_path=attn_path + f"i{inst_id}",
+                # attn_dump_path=None,
             )[0]
         pred = tokenizer.decode(output[context_length:], skip_special_tokens=True)
         pred = post_process(pred, model_name)
@@ -168,6 +252,8 @@ if __name__ == '__main__':
     # we design specific prompt format and max generation length for each task, feel free to modify them to optimize model output
     dataset2prompt = json.load(open("config/dataset2prompt.json", "r"))
     dataset2maxlen = json.load(open("config/dataset2maxlen.json", "r"))
+    dataset2prompt_comps = json.load(open("config/dataset2prompts_sliced_comps.json", "r"))
+    dataset2prompt_struct = json.load(open("config/dataset2prompts_sliced_struc.json", "r"))
     # predict on each dataset
     if not os.path.exists("pred"):
         os.makedirs("pred")
@@ -186,17 +272,29 @@ if __name__ == '__main__':
             out_path = f"pred/{model_name}/{dataset}.jsonl"
         prompt_format = dataset2prompt[dataset]
         max_gen = dataset2maxlen[dataset]
-        preds = get_pred(model, 
-                         tokenizer, 
-                         data, 
-                         max_length, 
-                         max_gen, 
-                         prompt_format, 
-                         dataset, 
-                         device, 
-                         model_name, 
-                         args.deepspeed,
-                         args.num_insts)
+        # preds = get_pred(model, 
+        #                  tokenizer, 
+        #                  data, 
+        #                  max_length, 
+        #                  max_gen, 
+        #                  prompt_format, 
+        #                  dataset, 
+        #                  device, 
+        #                  model_name, 
+        #                  args.deepspeed,
+        #                  args.num_insts)
+        preds = get_pred_sliced(model, 
+                                tokenizer, 
+                                data, 
+                                max_length, 
+                                max_gen, 
+                                dataset2prompt_struct[dataset],
+                                dataset2prompt_comps[dataset], 
+                                dataset, 
+                                device, 
+                                model_name, 
+                                args.deepspeed,
+                                args.num_insts)
         with open(out_path, "w", encoding="utf-8") as f:
             for pred in preds:
                 json.dump(pred, f, ensure_ascii=False)
